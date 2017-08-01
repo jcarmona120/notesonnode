8b2f6db74a8a184449a4d1a1fb5f460e33e66826
Jawann Carmona <jcarmona@ACTNY-IT-Jawann-Carmona.local>
2017-08-01 16:47:54 -0400

    Notes App made on Node

---
+++ notes.js
@@ -0,0 +1,70 @@
+console.log('Starting notes.js');
+
+const fs = require('fs');
+
+var fetchNotes = () => {
+	try  {
+
+		var noteString = fs.readFileSync('notes-data.json');
+		return JSON.parse(noteString)
+
+	} catch (e) {
+
+		return [];
+
+	}
+}
+
+var saveNotes = (notes) => {
+	fs.writeFileSync('notes-data.json', JSON.stringify(notes))
+}
+
+var addNote = (title, body) => {
+	var notes = fetchNotes();
+	var note = {
+		title,
+		body
+	}
+	var duplicateNotes = notes.filter((note) => note.title === title);
+
+
+	if (duplicateNotes.length === 0) {
+		notes.push(note)
+		saveNotes(notes);		
+		};
+	};
+
+var getAll = () => {
+	console.log('Getting all notes');
+}
+
+var getNote = (title) => {
+	var notes = fetchNotes();
+	var filteredNotes = notes.filter((note) => note.title === title);
+	return filteredNotes[0]
+}
+
+var removeNote = (title) => {
+	var notes = fetchNotes();
+	var filteredNotes = notes.filter((note) => note.title !== title)
+	saveNotes(filteredNotes);
+
+	return notes.length !== filteredNotes.length;
+}
+
+var logNote = (note) => {
+	console.log('Note created')
+  	console.log('---')
+  	console.log(`Title: ${note.title}`)
+  	console.log(`Body: ${note.body}`)
+}
+
+module.exports = {
+	// addNote: addNote
+	 addNote,
+	 // getAll: getAll
+	 getAll,
+	 getNote,
+	 removeNote,
+	 logNote  
+}
