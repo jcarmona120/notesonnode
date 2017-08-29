8927bcc9f8240152b7c10203400da6beb48f5b20
Jawann Carmona <jcarmona@ACTNY-IT-Jawann-Carmona.local>
2017-08-29 15:32:01 -0400

    hashing passwords

--- nodetodoapi/server/server.js
+++ nodetodoapi/server/server.js
@@ -90,7 +90,7 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = new Date().getTime();
   } else {
     body.completed = false;
-    body.compeltedAt = null;
+    body.completedAt = null;
   }
 
   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
@@ -120,9 +120,24 @@ app.post('/users', (req, res) => {
 
 
 
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
 app.get('/users/me', authenticate, (req, res) => {
-  res.send(req.user)
-})
+ 
+  res.send(req.user);
+
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
